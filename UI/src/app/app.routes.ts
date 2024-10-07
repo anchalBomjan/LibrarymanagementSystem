09d25aa37491d17dec55ab85895ae37027a93efe
anchalBomjan <anchal.percoidit@gmail.com>
2024-10-07 14:20:18 +0545

    adding approval requests

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -3,7 +3,10 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { BookStoreComponent } from './books/book-store/book-store.component';
 import { MaintenanceComponent } from './books/maintenance/maintenance.component';
+import { ReturnBookComponent } from './books/return-book/return-book.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { AllOrdersComponent } from './users/all-orders/all-orders.component';
+import { ApprovalRequestsComponent } from './users/approval-requests/approval-requests.component';
 import { ProfileComponent } from './users/profile/profile.component';
 import { UserOrdersComponent } from './users/user-orders/user-orders.component';
 
@@ -15,6 +18,9 @@ export const routes: Routes = [
     {path:"home",component:BookStoreComponent},
     {path:"my-orders",component:UserOrdersComponent},
     {path:"maintenance",component:MaintenanceComponent},
+    {path:"return-book",component:ReturnBookComponent},
+    {path:"approval-requests",component:ApprovalRequestsComponent},
     {path:"profile",component:ProfileComponent},
+    {path:"all-orders",component:AllOrdersComponent},
     { path: "**", component: PageNotFoundComponent } 
 ];
--- UI/src/app/users/all-orders/all-orders.component.ts
+++ UI/src/app/users/all-orders/all-orders.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Order } from '../../models/models';
+import { ApiService } from '../../shared/services/api.service';
 
 @Component({
   selector: 'all-orders',
@@ -6,5 +9,71 @@ import { Component } from '@angular/core';
   styleUrl: './all-orders.component.scss'
 })
 export class AllOrdersComponent {
+  columnsForPendingReturns: string[] = [
+    'orderId',
+    'userIdForOrder',
+    'userNameForOrder',
+    'bookId',
+    'orderDate',
+    'fineToPay',
+  ];
 
+  columnsForCompletedReturns: string[] = [
+    'orderId',
+    'userIdForOrder',
+    'userNameForOrder',
+    'bookId',
+    'orderDate',
+    'returnedDate',
+    'finePaid',
+  ];
+
+  showProgressBar: boolean = false;
+  ordersWithPendingReturns: Order[] = [];
+  ordersWithCompletedReturns: Order[] = [];
+
+  constructor(private apiService: ApiService, private snackBar: MatSnackBar) {
+    apiService.getOrders().subscribe({
+      next: (res: Order[]) => {
+        this.ordersWithPendingReturns = res.filter((o) => !o.returned);
+        this.ordersWithCompletedReturns = res.filter((o) => o.returned);
+      },
+      error: (err) => {
+        this.snackBar.open('No Orders Found', 'OK');
+      },
+    });
+  }
+
+  sendEmail() {
+    this.showProgressBar = true;
+    this.apiService.sendEmail().subscribe({
+      next: (res) => {
+        if (res === 'sent') {
+          this.snackBar.open(
+            'Emails have been Sent to respected Students!',
+            'OK'
+          );
+          this.showProgressBar = false;
+        } else {
+          this.snackBar.open('Emails have not been sent!', 'OK');
+          this.showProgressBar = false;
+        }
+      },
+    });
+  }
+
+  blockUsers() {
+    this.showProgressBar = true;
+    this.apiService.blockUsers().subscribe({
+      next: (res) => {
+        if (res === 'blocked') {
+          this.snackBar.open('Eligible Users Accounts were BLOCKED!', 'OK');
+          this.showProgressBar = false;
+        } else {
+          this.snackBar.open('Not BLOCKED!', 'OK');
+          this.showProgressBar = false;
+        }
+      },
+    });
+  }
 }
