d61f49e6d6d3178e9d008929dd1b537e80cdda96
anchalBomjan <anchal.percoidit@gmail.com>
2024-10-07 11:20:56 +0545

    adding maintenance feature of Admin Panel

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { BookStoreComponent } from './books/book-store/book-store.component';
+import { MaintenanceComponent } from './books/maintenance/maintenance.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 import { ProfileComponent } from './users/profile/profile.component';
 import { UserOrdersComponent } from './users/user-orders/user-orders.component';
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: "register", component: RegisterComponent },
     {path:"home",component:BookStoreComponent},
     {path:"my-orders",component:UserOrdersComponent},
+    {path:"maintenance",component:MaintenanceComponent},
     {path:"profile",component:ProfileComponent},
     { path: "**", component: PageNotFoundComponent } 
 ];
